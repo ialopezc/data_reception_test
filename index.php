837d7cf5e98b5b9e1a0de1a09c4dff129cf001b1
Isaac Alejandro López Castrejón <a.lopezc@hotmail.com>
2024-07-17 18:02:21 -0600

    Update index.php

--- index.php
+++ index.php
@@ -4,6 +4,19 @@ if (!$_REQUEST) {
 }
 
 try {
+    $path = 'logs';
+
+    if (!is_dir($path)) {
+        // El directorio no existe, así que lo creamos
+        if (mkdir($path, 0777, true)) {
+            echo "Directorio creado exitosamente.";
+        } else {
+            echo "Error al crear el directorio.";
+        }
+    } else {
+        echo "El directorio ya existe.";
+    }
+    
     $dateLog = date("Y-m-d__H-i-s");
 
     $logFile = fopen("logs/log-$dateLog.log", 'a') or die("Error creando archivo");
