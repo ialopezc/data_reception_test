5a06bc630b156fec82fd764b360fb20c06ea612c
Isaac Alejandro López Castrejón <a.lopezc@hotmail.com>
2024-07-25 10:56:02 -0600

    Update index.php

--- index.php
+++ index.php
@@ -1,27 +1,19 @@
 <?php
-// Verifica si no hay parámetros en la solicitud
-if (!$_REQUEST) {
-    var_dump(['validacion' => false, 'msg' => "Sin parámetros"]);
+$path = 'logs';
+
+// Verifica si el directorio 'logs' no existe y lo crea si es necesario
+if (!is_dir($path)) {
+    if (!mkdir($path, 0777, true)) {
+        throw new Exception("Error al crear el directorio.");
+    }
 }
 
+// Genera un nombre de archivo de log basado en la fecha y hora actual
+$dateLog = date("Y-m-d__H-i-s");
+$dateRegister = date("Y/m/d H:i:s");
+$logFile = fopen("$path/log-$dateLog.log", 'a') or die("Error creando archivo");
+
 try {
-    $path = 'logs';
-
-    // Verifica si el directorio 'logs' no existe y lo crea si es necesario
-    if (!is_dir($path)) {
-        if (mkdir($path, 0777, true)) {
-            var_dump("Directorio creado exitosamente.");
-        } else {
-            var_dump("Error al crear el directorio.");
-        }
-    } else {
-        var_dump("El directorio ya existe.");
-    }
-
-    // Genera un nombre de archivo de log basado en la fecha y hora actual
-    $dateLog = date("Y-m-d__H-i-s");
-    $logFile = fopen("logs/log-$dateLog.log", 'a') or die("Error creando archivo");
-
     // Obtiene el recurso solicitado
     $httpRefererName = null;
     $httpReferer = null;
@@ -52,28 +44,33 @@ try {
     // Guarda la referencia del solicitante en el archivo de log
     $datos['httpRefererName'] = $httpRefererName;
     $datos['httpReferer'] = $httpReferer;
-    fwrite($logFile, date("Y/m/d H:i:s") . " Solicitud desde = $httpRefererName: $httpReferer \n") or die("Error escribiendo en el archivo");
+    fwrite($logFile, "$dateRegister Solicitud desde = $httpRefererName: $httpReferer \n") or die("Error escribiendo en el archivo");
 
     // Guarda el método de la solicitud en el archivo de log
     $method = $_SERVER["REQUEST_METHOD"];
     $datos['method'] = $method;
-    fwrite($logFile, date("Y/m/d H:i:s") . " Método = $method \n") or die("Error escribiendo en el archivo");
+    fwrite($logFile, "$dateRegister Método = $method \n") or die("Error escribiendo en el archivo");
 
     // Guarda el puerto de la solicitud en el archivo de log
     $httpRefererPort = $_SERVER['SERVER_PORT'];
     $datos['httpRefererPort'] = $httpRefererPort;
-    fwrite($logFile, date("Y/m/d H:i:s") . " Puerto = $httpRefererPort \n") or die("Error escribiendo en el archivo");
+    fwrite($logFile, "$dateRegister Puerto = $httpRefererPort \n") or die("Error escribiendo en el archivo");
 
     // Guarda el tipo de datos de la solicitud en el archivo de log
     $typeData = gettype($_REQUEST);
     $datos['typeData'] = $typeData;
-    fwrite($logFile, date("Y/m/d H:i:s") . " Tipo de datos = $typeData \n") or die("Error escribiendo en el archivo");
+    fwrite($logFile, "$dateRegister Tipo de datos = $typeData \n") or die("Error escribiendo en el archivo");
+
+    // Verifica si no hay parámetros en la solicitud
+    if (!$_REQUEST) {
+        throw new Exception("Sin parámetros");
+    }
 
     // Guarda cada uno de los campos incluidos en la solicitud en el archivo de log
     foreach ($_REQUEST as $name => $value) {
         $assignment = $name . " = '" . trim($value) . "'";
         $datos[$name] = trim($value);
-        fwrite($logFile, date("Y/m/d H:i:s") . " $assignment \n") or die("Error escribiendo en el archivo");
+        fwrite($logFile, "$dateRegister $assignment \n") or die("Error escribiendo en el archivo");
     }
 
     // Cierra el archivo de log
@@ -82,5 +79,6 @@ try {
     var_dump(['validacion' => true, 'msg' => "Datos recibidos", 'datos' => $datos]);
 } catch (Exception $e) {
     // Manejo de excepciones
+    fwrite($logFile, "$dateRegister Error = " .  $e->getMessage() . " \n") or die("Error escribiendo en el archivo");
     var_dump(['validacion' => false, 'msg' => "Error: " . $e->getMessage()]);
 }
